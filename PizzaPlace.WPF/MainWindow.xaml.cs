ee904a8b8ee57dab370c0175cd980ef2b38d4c70
selindimitri@gmail.com <selindimitri@gmail.com>
2022-01-01 17:40:55 +0300

    Добавьте файлы проекта.

---
+++ PizzaPlace.WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PizzaPlace.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
