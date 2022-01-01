a9c836e4e340494b0fde7a9ce2f7821ad3db726b
selindimitri@gmail.com <selindimitri@gmail.com>
2022-01-01 21:24:29 +0300

    Add Admin class

---
+++ PizzaPlace.WPF/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaPlace.WPF.Models
+{
+    internal class Admin : Person
+    {
+        public int Rating { get; private set; }
+
+        public Admin(int id, string login, string password, string firstName,
+                    string secondName) : base(id, login, password, firstName, secondName)
+        {
+            Rating = 1;
+        }
+    }
+}
