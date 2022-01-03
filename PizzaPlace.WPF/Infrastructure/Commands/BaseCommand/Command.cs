1951a09850b0ffd2ae5050a8b5407d2df4fa5d04
selindimitri@gmail.com <selindimitri@gmail.com>
2022-01-03 13:43:34 +0300

    Add Command class

--- PizzaPlace.WPF/Infrastructure/Commands/BaseCommand/Command.cs
+++ PizzaPlace.WPF/Infrastructure/Commands/BaseCommand/Command.cs
@@ -1,7 +1,18 @@
-﻿namespace PizzaPlace.WPF.Infrastructure.Commands.BaseCommand
-{
-    class Command
-    {
+﻿using System;
+using System.Windows.Input;
 
+namespace PizzaPlace.WPF.Infrastructure.Commands.BaseCommand
+{
+    abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
     }
 }
