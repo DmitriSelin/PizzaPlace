22f3e6bd7f33a2d2de4365a76cbf9cd5f9b1b3d7
selindimitri@gmail.com <selindimitri@gmail.com>
2022-01-03 13:37:20 +0300

    Add ViewModel (base class for WindowsViewModels)

--- PizzaPlace.WPF/ViewModels/Base/ViewModel.cs
+++ PizzaPlace.WPF/ViewModels/Base/ViewModel.cs
@@ -1,8 +1,24 @@
-﻿
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace PizzaPlace.WPF.ViewModels.Base
 {
-    class ViewModel
+    class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
