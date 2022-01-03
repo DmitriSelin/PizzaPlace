32411ee1ba7d72bb6f2ef732e9c9aa81a01c7721
selindimitri@gmail.com <selindimitri@gmail.com>
2022-01-03 13:54:39 +0300

    Add LambdaCommand class

--- PizzaPlace.WPF/Infrastructure/Commands/LambdaCommand.cs
+++ PizzaPlace.WPF/Infrastructure/Commands/LambdaCommand.cs
@@ -1,8 +1,24 @@
-﻿
+﻿using PizzaPlace.WPF.Infrastructure.Commands.BaseCommand;
+using System;
+
 namespace PizzaPlace.WPF.Infrastructure.Commands
 {
-    class LambdaCommand
+    class LambdaCommand : Command
     {
+        private readonly Action<object> execute;
 
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter) => execute(parameter);
     }
 }
