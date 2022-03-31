10bb3de1d3dbd01fa4bac6d494d8d01d7f6af8a6
selindimitri@gmail.com <selindimitri@gmail.com>
2022-03-31 20:42:12 +0300

    Add filter to Pizza Search

--- PizzaPlace.WPF/Views/UserControls/HomeView.xaml.cs
+++ PizzaPlace.WPF/Views/UserControls/HomeView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using PizzaPlaceDB.DAL.Entities;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace PizzaPlace.WPF.Views.UserControls
 {
@@ -7,6 +9,29 @@ namespace PizzaPlace.WPF.Views.UserControls
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        private void FilterFoodCollection(object sender, FilterEventArgs e)
+        {
+            if (e.Item is not Food food) return;
+
+            if (food.Name is null) return;
+
+            var filterText = FoodFilterText.Text;
+
+            if (filterText.Length == 0) return;
+
+            if (food.Name.Contains(filterText, System.StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnFoodFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            var foodCollection = (CollectionViewSource)textBox.FindResource("FoodCollection");
+
+            foodCollection.View.Refresh();
         }
     }
 }
