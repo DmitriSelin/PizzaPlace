7cd293e55769900d0395ab05f351a5fb1476f287
selindimitri@gmail.com <selindimitri@gmail.com>
2022-04-06 23:09:34 +0300

    Delete nonworking code

--- PizzaPlace.WPF/ViewModels/StatisticViewModel.cs
+++ PizzaPlace.WPF/ViewModels/StatisticViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using PizzaPlace.BL.Interfaces;
 using PizzaPlace.WPF.ViewModels.Base;
 using PizzaPlaceDB.DAL.Entities;
-using System.Collections.ObjectModel;
 
 namespace PizzaPlace.WPF.ViewModels
 {
@@ -9,12 +8,6 @@ namespace PizzaPlace.WPF.ViewModels
     {
         private readonly IRepository<Basket> baskets;
         private readonly IRepository<Food> food;
-
-        public ObservableCollection<decimal> Prices { get; }
-
-        public ObservableCollection<Basket> Baskets { get; }
-
-        public ObservableCollection<Food> Food { get; }
 
         private User user
         {
@@ -25,26 +18,6 @@ namespace PizzaPlace.WPF.ViewModels
         {
             baskets = _baskets;
             food = _food;
-
-            Baskets = new ObservableCollection<Basket>(baskets.Items);
-            Food = new ObservableCollection<Food>(food.Items);
-            Prices = new ObservableCollection<decimal>();
-
-            GetPrices();
-        }
-
-        private void GetPrices()
-        {
-            foreach(var basket in Baskets)
-            {
-                foreach(var product in Food)
-                {
-                    if (basket.FoodId == product.Id && basket.UserId == user.Id)
-                    {
-                        Prices.Add(product.Price);
-                    }
-                }
-            }
         }
     }
 }
