af5c208c0bcf8f566192c41d95f65cd931171d12
selindimitri@gmail.com <selindimitri@gmail.com>
2022-04-09 12:04:57 +0300

    ChangeServiceRegistrator

--- PizzaPlace.BL/Services/ServiceRegistrator.cs
+++ PizzaPlace.BL/Services/ServiceRegistrator.cs
@@ -5,6 +5,7 @@ namespace PizzaPlace.BL.Services
 {
     public static class ServiceRegistrator
     {
+        /// <summary>Adding services</summary>
         public static IServiceCollection AddServices(this IServiceCollection services) => services
             .AddTransient<IUserService, UserService>()
             .AddTransient<IBasketService, BasketService>()
