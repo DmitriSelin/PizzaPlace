98758f36fea5a1f7d88c27b72692b4211b38b23c
selindimitri@gmail.com <selindimitri@gmail.com>
2023-11-29 20:51:08 +0300

    Update project

--- PizzaPlaceDB.DAL/RepositoryRegistrator.cs
+++ PizzaPlaceDB.DAL/RepositoryRegistrator.cs
@@ -7,13 +7,11 @@ namespace PizzaPlaceDB.DAL
     public static class RepositoryRegistrator
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services) => services
-            .AddTransient<IRepository<Admin>, DbRepository<Admin>>()
             .AddTransient<IRepository<Basket>, BasketRepository>()
             .AddTransient<IRepository<Bonus>, DbRepository<Bonus>>()
             .AddTransient<IRepository<Category>, DbRepository<Category>>()
             .AddTransient<IRepository<Discount>, DbRepository<Discount>>()
             .AddTransient<IRepository<Food>, FoodRepository>()
-            .AddTransient<IRepository<User>, UserRepository>()
-            ;
+            .AddTransient<IRepository<User>, UserRepository>();
     }
 }
